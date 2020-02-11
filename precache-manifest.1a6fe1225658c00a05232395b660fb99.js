00757c6f147f12110a5668af62a6201bad5ddd54
Sergei Korchemkin <hellk.pub@gmail.com>
2020-02-11 05:56:52 +0300

    Updates

---
+++ precache-manifest.1a6fe1225658c00a05232395b660fb99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8fda303199280a7a1fa0b4f84f4c64",
+    "url": "/rest-countries-api-with-color-theme-switcher/index.html"
+  },
+  {
+    "revision": "892cb257563ab6f7cb30",
+    "url": "/rest-countries-api-with-color-theme-switcher/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "67ec0ca4789c79e47f42",
+    "url": "/rest-countries-api-with-color-theme-switcher/static/js/2.3aa2cc28.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/rest-countries-api-with-color-theme-switcher/static/js/2.3aa2cc28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "892cb257563ab6f7cb30",
+    "url": "/rest-countries-api-with-color-theme-switcher/static/js/main.bd896346.chunk.js"
+  },
+  {
+    "revision": "e1dbcd33a97bf63e6482",
+    "url": "/rest-countries-api-with-color-theme-switcher/static/js/runtime-main.2236c141.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rest-countries-api-with-color-theme-switcher/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rest-countries-api-with-color-theme-switcher/precache-manifest.1a6fe1225658c00a05232395b660fb99.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rest-countries-api-with-color-theme-switcher/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
