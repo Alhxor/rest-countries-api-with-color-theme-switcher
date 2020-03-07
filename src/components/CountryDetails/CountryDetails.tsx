771ab7be78f739b53d89f8a10265efa30495ed3b
Sergei Korchemkin <hellk.pub@gmail.com>
2020-03-08 00:32:26 +0300

    Refactor class names in CountryDetails to match BEM

--- src/components/CountryDetails/CountryDetails.tsx
+++ src/components/CountryDetails/CountryDetails.tsx
@@ -13,35 +13,37 @@ export const CountryDetails: React.FC<Props> = props => {
   const { colors } = useContext(ThemeContext)
   return (
     <>
-      <div className="country-details">
-        <div className="country-details__controls">
-          <button className="controls__back" onClick={() => props.goBack()}>
+      <div className="c-country-details">
+        <div className="c-controls">
+          <button className="c-controls__back" onClick={() => props.goBack()}>
             Back
           </button>
         </div>
-        <div className="country-details__content">
-          <figure className="content__flag">
+        <div className="c-country-details__content">
+          <figure className="c-country-details__flag">
             <img
               src={props.flag}
               alt={`${props.name}'s flag.`}
-              className="flag__img"
+              className="c-country-details__flag-img"
             />
           </figure>
-          <div className="content__text">
-            <header className="text__header">{props.name}</header>
-            <ul className="text__details-list">
-              {/* Convert this list to map */}
-              <li className="details-list__item">
+          <div className="c-country-details__text-info">
+            <header className="c-country-details__header">{props.name}</header>
+            <ul className="c-details-list">
+              {/* TODO: convert this list to map */}
+              <li className="c-details-list__item">
                 Native Name: {props.nativeName}
               </li>
-              <li className="details-list__item">
+              <li className="c-details-list__item">
                 Population: {props.population}
               </li>
             </ul>
-            <div className="text__country-borders">
-              Border Countries:
-              <ul className="country-borders__name-list">
-                <li className="name-list__item">
+            <div className="c-country-borders">
+              <header className="c-country-borders__header">
+                Border Countries:
+              </header>
+              <ul className="c-country-borders__list">
+                <li className="c-country-borders__item">
                   {props.borders && props.borders[0]}
                 </li>
               </ul>
